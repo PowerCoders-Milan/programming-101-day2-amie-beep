48a9b15afbd0eb2d5a86ca4c4133b40d2378600d
amie senghore <amiesenghore1@gmail.com>
2021-02-19 13:05:41 +0100

    exercise2

--- 02-Counting-Characters/counting_characters.js
+++ 02-Counting-Characters/counting_characters.js
@@ -1,8 +1,17 @@
 //insert your pseudocode below
 /*
 
-here
+create a variable to store the string "text"
+and ask a user to enter the string
+create another variable to store the length of the text
+print the text that the user entered and the length of the text
 
 */
 
 //insert your code below
+var text;
+
+
+text = window.prompt('Type some text');
+window.alert(text.length);
+window.alert(text);
